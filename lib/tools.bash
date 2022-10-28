c69764cc52abf1fc76afff09e597ef6744c36ca9
Raj Perera <rajiteh@gmail.com>
2022-10-28 17:52:52 +0000

    wip;

---
+++ lib/tools.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+declare -A toolnames
+toolnames["bats"]="bats-core/bats-core"
+toolnames["bats-assert"]="ztombol/bats-assert"
+toolnames["bats-support"]="ztombol/bats-support"
+toolnames["bats-mock"]="rajiteh/bats-mock"
+
+__dirname="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+toolname="$(basename "$(dirname "${__dirname}")")"
+
+get_tool_name() {
+    # echo "${toolname}"
+    echo "bats-mock"
+}
+
+get_repo_for_tool() {
+    echo "${toolnames[$1]}"
+}
